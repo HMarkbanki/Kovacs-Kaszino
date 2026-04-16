5a188d339f2ed8013bde7bf74e4e81ae178de229
mark <horvath.mark.imre.2022i@bankitatabanya.hu>
2026-04-16 13:55:56 +0200

    Nyerogep javitas

--- src/Nyerogep.java
+++ src/Nyerogep.java
@@ -4,6 +4,9 @@ public class Nyerogep extends Jatekgep {
     public Nyerogep(int azonosito, String nev, double maximumTet, double minimumTet) {
         super(azonosito, nev, maximumTet, minimumTet);
         setJackpotHuzas(0);
+        this.setJatszottKorok(0);
+        this.setOsszesBevet(0);
+        this.setOsszesKifizetes(0);
     }
 
     public int getJackpotHuzas() {
@@ -22,22 +25,28 @@ public class Nyerogep extends Jatekgep {
         String s2 = huzosSzimbolum();
         String s3 = huzosSzimbolum();
         if (tetellenorzes(tet)){
+            osszesKifizetes-=tet;
             setJatszottKorok(this.jatszottKorok+1);
             if (s1.equals(s2) && s1.equals(s3) && s1.equals("JACKPOT")){
                 System.out.println("[ "+s1+" | "+s2+" | "+s3+" ] → +"+tet*50+" FT");
+                osszesKifizetes+=tet*50;
                 this.jackpotHuzas++;
                 return tet*50;
             } else if (s1.equals(s2) && s1.equals(s3)) {
                 System.out.println("[ "+s1+" | "+s2+" | "+s3+" ] → +"+tet*10+" FT");
+                osszesKifizetes+=tet*10;
                 return tet*10;
             } else if (s1.equals(s2)) {
                 System.out.println("[ "+s1+" | "+s2+" | "+s3+" ] → +"+tet*2+" FT");
+                osszesKifizetes+=tet*2;
                 return tet*2;
             }else if (s1.equals(s3)) {
                 System.out.println("[ "+s1+" | "+s2+" | "+s3+" ] → +"+tet*2+" FT");
+                osszesKifizetes+=tet*2;
                 return tet*2;
             }else if (s2.equals(s3)) {
                 System.out.println("[ "+s1+" | "+s2+" | "+s3+" ] → +"+tet*2+" FT");
+                osszesKifizetes+=tet*2;
                 return tet*2;
             }else {
                 System.out.println("[ "+s1+" | "+s2+" | "+s3+" ] → +"+tet*0+" FT");
